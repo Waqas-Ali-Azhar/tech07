6b712afd44c03b81b98381948e18d8edb5aaa7b6
Waqas Ali <waqas_111@hotmail.com>
2021-01-29 10:22:31 +0500

    added new stylesheet and paragraph

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html>
 <head>
   <title>Git Introducton</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
   <h1>Git Command</h1>
 
-
+  <p>This is new code and new paragraph</p>
 
 </body>
 </html>
